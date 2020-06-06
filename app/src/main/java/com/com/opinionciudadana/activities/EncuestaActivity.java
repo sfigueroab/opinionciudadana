f52091a18fcbfa68c22b6a6444405fb166a8fde7
Jose Villasmil <jose.villasmil@doctum.net>
2020-06-06 14:34:34 -0400

    validación de respuestas

--- app/src/main/java/com/com/opinionciudadana/activities/EncuestaActivity.java
+++ app/src/main/java/com/com/opinionciudadana/activities/EncuestaActivity.java
@@ -1,19 +1,33 @@
 package com.com.opinionciudadana.activities;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
 import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
 import com.com.opinionciudadana.R;
 import com.com.opinionciudadana.model.Encuesta;
 import com.com.opinionciudadana.model.Respuesta;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EncuestaActivity extends DefaultActivity {
 
@@ -21,8 +35,9 @@ public class EncuestaActivity extends DefaultActivity {
     private Button yes;
     private Button no;
     private String key;
-
     private Pie pie;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +56,7 @@ public class EncuestaActivity extends DefaultActivity {
     @Override
     public void createViews() {
         super.createViews();
-        // chart = findViewById(R.id.any_chart_view);
+        chart = findViewById(R.id.any_chart_view);
         yes = findViewById(R.id.si);
         yes.setOnClickListener(view -> sendYes(view));
         no = findViewById(R.id.no);
@@ -58,9 +73,18 @@ public class EncuestaActivity extends DefaultActivity {
             if(queryDocumentSnapshots.isSuccessful()) {
                 DocumentSnapshot encuesta = queryDocumentSnapshots.getResult();
                 Encuesta miEncuesta = encuesta.toObject(Encuesta.class);
-                // createChart(miEncuesta);
-                TextView tituloTextView = findViewById(R.id.textView8);
-                tituloTextView.setText(miEncuesta.getPregunta().toString());
+                createChart(miEncuesta);
+
+
+                Button si = findViewById(R.id.si);
+                si.setText(miEncuesta.getPreguntas().get(0).toString());
+                si.setEnabled(false);
+
+                Button no = findViewById(R.id.no);
+                no.setText(miEncuesta.getPreguntas().get(1).toString());
+                no.setEnabled(false);
+
+                validarUsuario(key);
             } else {
             }
         });
@@ -78,9 +102,8 @@ public class EncuestaActivity extends DefaultActivity {
                 miEncuesta.getResultados().set(0, respuestasActuales);
                 firestoreManager.setField("encuestas", key, "resultados", miEncuesta.getResultados(), task -> {
                     addData(miEncuesta);
-                   // chart.refreshDrawableState();
+                    chart.refreshDrawableState();
                     this.setRespuesta(key);
-
                 });
             }
         });
@@ -96,13 +119,49 @@ public class EncuestaActivity extends DefaultActivity {
                 miEncuesta.getResultados().set(1, respuestasActuales);
                 firestoreManager.setField("encuestas", key, "resultados", miEncuesta.getResultados(), task -> {
                     addData(miEncuesta);
-                   // chart.refreshDrawableState();
+                    chart.refreshDrawableState();
                     this.setRespuesta(key);
                 });
             }
         });
     }
 
+    public void validarUsuario(String encuestaId) {
+        firestoreManager.getDocument("users", authManager.getUser().getId(), task -> {
+            if(!task.isSuccessful()) {
+                return;
+            }
+            DocumentSnapshot _respuesta = task.getResult();
+            Respuesta respuesta = _respuesta.toObject(Respuesta.class);
+
+            if(respuesta == null || respuesta.getEncuestas() == null) {
+                Button si = findViewById(R.id.si);
+                si.setEnabled(true);
+
+                Button no = findViewById(R.id.no);
+                no.setEnabled(true);
+
+                return;
+            }
+
+            boolean activar = true;
+            for (String res : respuesta.getEncuestas()) {
+                if(res == encuestaId) {
+                    activar = false;
+                }
+            }
+
+            if(activar) {
+                Button si = findViewById(R.id.si);
+                si.setEnabled(true);
+
+                Button no = findViewById(R.id.no);
+                no.setEnabled(true);
+            }
+
+
+        });
+    }
 
     public void setRespuesta(String key) {
         firestoreManager.getDocument("users", authManager.getUser().getId(), task1 -> {
@@ -129,11 +188,15 @@ public class EncuestaActivity extends DefaultActivity {
                 Log.i("debug", "llegue");
             });
         });
+        validarUsuario(key);
+        new AlertDialog.Builder(EncuestaActivity.this)
+                .setTitle("Muchas gracias")
+                .setMessage("Su respuesta ha sido guardada con éxito").show();
     }
 
 
     public void createChart(Encuesta encuesta) {
-     /*   pie = AnyChart.pie();
+        pie = AnyChart.pie();
 
         pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
             @Override
@@ -143,8 +206,9 @@ public class EncuestaActivity extends DefaultActivity {
         });
 
         List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry(encuesta.getEncuestas().get(0), encuesta.getResultados().get(0)));
-        data.add(new ValueDataEntry(encuesta.getEncuestas().get(1), encuesta.getResultados().get(1)));
+        Log.i("debug", "Resultados"  + encuesta.getResultados().get(0).toString());
+        data.add(new ValueDataEntry(encuesta.getPreguntas().get(0), encuesta.getResultados().get(0)));
+        data.add(new ValueDataEntry(encuesta.getPreguntas().get(1), encuesta.getResultados().get(1)));
 
         pie.data(data);
 
@@ -162,7 +226,7 @@ public class EncuestaActivity extends DefaultActivity {
                 .itemsLayout(LegendLayout.HORIZONTAL)
                 .align(Align.CENTER);
         chart.setChart(pie);
-        */
+
 
     }
 
